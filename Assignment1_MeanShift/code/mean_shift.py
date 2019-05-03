1eb26d3c901d2b3627ea80c296b5297a87ad270a
Kevin Trebing <Kevin.Trebing@gmx.net>
2019-05-03 19:57:55 +0200

    Something is happening

--- Assignment1_MeanShift/code/mean_shift.py
+++ Assignment1_MeanShift/code/mean_shift.py
@@ -5,6 +5,31 @@ import scipy.io as sio
 from scipy.spatial.distance import cdist
 
 
+def plotclusters3D(data, labels, peaks):
+    """
+    Plots the modes of the given image data in 3D by coloring each pixel
+    according to its corresponding peak.
+
+    Args:
+        data: image data in the format [number of pixels]x[feature vector].
+        labels: a list of labels, one for each pixel.
+        peaks: a list of vectors, whose first three components can
+        be interpreted as BGR values.
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    bgr_peaks = np.array(peaks[:, 0:3], dtype=float)
+    rgb_peaks = bgr_peaks[...,::-1]
+    rgb_peaks /= 255.0
+    for idx, peak in enumerate(rgb_peaks):
+        color = np.random.uniform(0, 1, 3)
+        #TODO: instead of random color, you can use peaks when you work on actual images
+        # color = peak
+        cluster = data[np.where(labels == idx)[0]].T
+        ax.scatter(cluster[0], cluster[1], cluster[2], c=[color], s=.5)
+    plt.show()
+
+
 def findpeak(data, idx, r):
     dist = cdist(data[idx].reshape(1, -1), data)
     # print(data.shape)
@@ -19,31 +44,57 @@ def meanshift(data, r):
     # implementation of meanshift, we will consider two peaks to be the same if the distance between
     # them is smaller than r/2. Also, if the peak of a data point is found to already exist in peaks then for
     # simplicity its computed peak is discarded and it is given the label of the associated peak in peaks.
-    data = data.T
+    # data = data.T
     labels = np.zeros(len(data))
     peaks = np.zeros(data.shape)
+    numLabels = 0
     t = 0.01
-    # repeat until convergence
-    for i, point in enumerate(data):
-        newPeak = findpeak(data, i, r)
-        print(newPeak.shape)
-        # get distance from peak to other peaks and maybe merge
-        peakDistances = cdist(newPeak.reshape(1, -1), peaks)
-        print(peakDistances.shape)
-        if peakDistances < r/2:
-            pass
 
+    runs = 0
+    # repeat until convergence
+    converged = False
+
+    while not converged:
+        for i, point in enumerate(data):
+            newPeak = findpeak(data, i, r)
+            # print(newPeak.shape)
+            # get distance from peak to other peaks and maybe merge
+            peakDistances = cdist(newPeak.reshape(1, -1), peaks)[0]
+            # print(peakDistances.shape)
+
+            samePeaks = peakDistances < r/2
+            # print(samePeaks)
+            if np.sum(samePeaks) > 0:
+                # print(np.mean(peakDistances[samePeaks], axis=0).shape)
+                peaks[samePeaks] = np.mean(peakDistances[samePeaks], axis=0)
+                labels[samePeaks] = labels[samePeaks][0]
+            else:
+                # Peak is different enough to get assigned a new label
+                numLabels += 1
+                peaks[i] = newPeak
+                labels[i] = numLabels
+        runs += 1
+
+        print(runs)
+        if runs > 20:
+            converged = True
 
     return labels, peaks
 
 
 points = sio.loadmat("../data/pts.mat")['data']
 print("Data shape", points.shape)
+points = points.T
+
+# print(points[:, 0], np.min(points[:, 0]), np.max(points[:, 0]))
+# print(points[:, 1], np.min(points[:, 1]), np.max(points[:, 1]))
+# print(points[:, 2], np.min(points[:, 2]), np.max(points[:, 2]))
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# ax.scatter3D(points[:, 0], points[:, 1], points[:, 2])
+# plt.show()
 
 labels, peaks = meanshift(points, 2)
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.scatter(points[0, :], points[1, :], points[2, :])
-plt.show()
+print(f'Final labels: {labels} and peaks: {peaks}')
+plotclusters3D(points, labels, peaks)
