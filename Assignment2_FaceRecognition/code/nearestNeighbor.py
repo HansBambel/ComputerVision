d3d141ed3efaf7718debff3aabadded1fa476a01
Kevin Trebing <Kevin.Trebing@gmx.net>
2019-05-17 16:11:37 +0200

    reconstruction looks similar

--- Assignment2_FaceRecognition/code/nearestNeighbor.py
+++ Assignment2_FaceRecognition/code/nearestNeighbor.py
@@ -11,17 +11,18 @@ def eigenfaces_train(training_images, k=10):
     face_mean = np.mean(training_images, axis=0)
     # plt.imshow(face_mean.reshape(32, 32).T, cmap='gray')
     # plt.show()
-    # TODO eigenvectors sohuld have size 1024!!
+    # TODO eigenvectors should have size 1024!!
     face_cov = np.cov(training_images)
-    # face_cov = 1/len(training_images) * np.matmul(training_images, training_images.T)
+    # face_cov = 1/len(training_images) * np.matmul(training_images-face_mean, (training_images-face_mean).T)
     eigenvals, eigenvecs = np.linalg.eig(face_cov)
     # use the eigenvectors with the biggest eigenvalues (they are sorted already)
-    eigenvecsToUse = eigenvecs[:k]
-    projectionMatrix = np.dot(eigenvecsToUse, training_images-face_mean)
-    return projectionMatrix, face_mean, eigenvecsToUse
+    eigenvecsToUse = np.dot(eigenvecs, training_images)[:k]
+    # projectionMatrix = np.dot(eigenvecsToUse, (training_images-face_mean).T)
+    return face_mean, eigenvecsToUse
 
 
-numberOfTrainingImages = 3
+numberOfTrainingImages = 7
+# Note: indices are done for Matlab (aRraYs stArT aT 1...) so we need to subtract 1
 trainIdx = sio.loadmat(f"../data/{numberOfTrainingImages}Train/{numberOfTrainingImages}.mat")['trainIdx']-1
 testIdx = sio.loadmat(f"../data/{numberOfTrainingImages}Train/{numberOfTrainingImages}.mat")['testIdx']-1
 
@@ -37,7 +38,7 @@ print(data_gnd.shape)
 print(data_fea.shape)
 # Normalize data
 features = data_fea / 255
-print(features)
+# print(features)
 
 # plt.subplot(121)
 # plt.title("Mean face")
@@ -50,11 +51,11 @@ print(features)
 trainFaces = np.squeeze(features[trainIdx])
 testFaces = np.squeeze(features[testIdx])
 # Train with images to get projectionMatrix
-k = 6
-projectionMatrix, face_mean, eigenvecs = eigenfaces_train(trainFaces, k=k)
+k = 20
+face_mean, eigenvecs = eigenfaces_train(trainFaces, k=k)
 
-plt.title("Eigenfaces")
-for i, face in enumerate(projectionMatrix):
+plt.suptitle("Eigenfaces")
+for i, face in enumerate(eigenvecs):
     plt.subplot(2, k/2, i+1)
     plt.title(f"{i+1}")
     plt.axis('off')
@@ -68,7 +69,10 @@ plt.title("Original Image")
 plt.imshow(imageToReconstruct.reshape(32, 32).T, cmap='gray')
 plt.subplot(122)
 plt.title("Reconstructed Image")
-plt.imshow((face_mean + np.dot(eigenvecs[0], projectionMatrix)).reshape(32, 32).T, cmap='gray')
+reconstructed = face_mean + np.sum(eigenvecs* (imageToReconstruct-face_mean), axis=0)
+plt.imshow(reconstructed.reshape(32, 32).T, cmap='gray')
 plt.show()
-# TODO dont forget to substract the mean of the data
+error = np.sum(np.abs(imageToReconstruct-reconstructed))
+print("Error ", error)
+
 # TODO use euclidean distance for NN and take k neighbors (instead of radius)
